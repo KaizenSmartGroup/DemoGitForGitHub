3061ce6730daab3e42e6438a14d01b620f2909b7
mugale <alessiothemugna@gmail.com>
2016-05-13 16:11:57 +0200

    i have to commit all project essentials

---
+++ DemoGitForGitHub/src/main/java/it/cosesa/DemoGitForGitHubApplication.java
@@ -0,0 +1,12 @@
+package it.cosesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitForGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitForGitHubApplication.class, args);
+	}
+}
